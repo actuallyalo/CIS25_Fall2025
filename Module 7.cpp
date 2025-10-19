b23841772ccef25ef4dd8e165420c8c11b0b1228
Alex Lopez <alexlopez86@gmail.com>
2025-10-18 21:26:59 -0700

    Refactor Module 7.cpp to improve code structure

--- Module 7.cpp
+++ Module 7.cpp
@@ -1,11 +1,10 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
+#include <vector>
 #include <algorithm>
-#include <iomanip>
-using namespace std;
 
 struct Item {
-    string name;
+    std::string name;
     int id;
 };
 
